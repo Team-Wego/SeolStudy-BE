f63ebabc3c185553d3d2e0e81a2a4efa2300cd30
taehuni <alsxogns72@gmail.com>
2026-02-06 20:38:36 +0900

    feat: #17 회원 개인 정보 조회/수정 API 구현

--- src/main/java/com/wego/seolstudybe/member/controller/MemberController.java
+++ src/main/java/com/wego/seolstudybe/member/controller/MemberController.java
@@ -2,10 +2,15 @@ package com.wego.seolstudybe.member.controller;
 
 import com.wego.seolstudybe.member.dto.LoginRequest;
 import com.wego.seolstudybe.member.dto.LoginResponse;
+import com.wego.seolstudybe.member.dto.MemberResponse;
+import com.wego.seolstudybe.member.dto.UpdateMemberRequest;
 import com.wego.seolstudybe.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +25,19 @@ public class MemberController {
     @PostMapping("/login")
     public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest request) {
         LoginResponse response = memberService.login(request);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<MemberResponse> getMember(@PathVariable int id) {
+        MemberResponse response = memberService.getMember(id);
+        return ResponseEntity.ok(response);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<MemberResponse> updateMember(@PathVariable int id,
+                                                       @RequestBody UpdateMemberRequest request) {
+        MemberResponse response = memberService.updateMember(id, request);
         return ResponseEntity.ok(response);
     }
 }
